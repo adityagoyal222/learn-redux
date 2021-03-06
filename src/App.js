a2fa59c5f6acb422c17792087d7997a742590ab2
Aditya Goyal <goyaladitya85@gmail.com>
2021-03-06 20:11:02 +0545

    used provider to inherit state into react componenets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import {useSelector} from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.counter);
+
   return (
     <div className="App">
+      <h1>Counter {counter}</h1> 
+      { isLogged ? <h3>Valuable information I shouldn't see </h3> : '' }
       
     </div>
   );
